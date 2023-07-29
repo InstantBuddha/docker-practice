2fcab2224c62c4063cc160551333a1616ee2ccaf
InstantBuddha <instantbuddha@gmail.com>
2023-07-29 10:00:14 +0200

    Add basic php

---
+++ test-connection.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "testUser";
+$password = "testPassword1234";
+$dbname = "test_database";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully!\n";
+$conn->close();
+?>
