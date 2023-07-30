7181847ca7e90f267d4646bb825f0a305806c6af
InstantBuddha <instantbuddha@gmail.com>
2023-07-30 07:09:25 +0200

    Verify installation working

--- app/index.php
+++ app/index.php
@@ -1,3 +1,21 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Test connection</title>
+</head>
+<body>
+    <?php
+    if (isset($_POST['btnClicked'])) {
+        if (extension_loaded('mysqli')) {
+            echo "MySQLi extension is enabled.";
+        } else {
+            echo "MySQLi extension is not enabled.";
+        }
+    }
+    ?>
 
-echo 'Hello world!';
+    <form method="post">
+        <input type="submit" name="btnClicked" value="Check connection">
+    </form>
+</body>
+</html>
