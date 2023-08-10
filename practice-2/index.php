21dddd8aa0ba47c37a4a23cee0047b1d8b3bd8a5
Daniel Marton <instantbuddha@gmail.com>
2023-08-10 12:11:50 +0200

    Add working alpine

---
+++ practice-2/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Test connection</title>
+</head>
+<body>
+    <h1>Php practice 2</h1>
+    <?php
+    echo "Php is running";
+    ?>
+
+    
+</body>
+</html>
